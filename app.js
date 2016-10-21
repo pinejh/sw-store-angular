8f8e99c270b9a5f0f950f0e4823d0be5757d61e5
pinejh <pinejh95@gmail.com>
2016-10-21 16:24:36 -0400

    loaded sabers thru angular

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var storeApp = angular.module('storeApp', []);
 
-storeApp.controller('storeCtrl', function storeCtrl ($scope) {
- $scope.sabers = sabers;
+storeApp.controller('storeCtrl', function storeCtrl($scope) {
+ $scope.saberRows = function () {
+  var row = [], data = [], i;
+  for (i = 0; i < sabers.length; i += 1) {
+   if (i % 3 === 0 && i !== 0) {
+    row.push([data[0], data[1], data[2]]);
+    data.splice(0, data.length);
+   }
+   data.push(sabers[i]);
+   if (i === sabers.length - 1) {
+    if (data[1] == undefined) row.push([data[0]]);
+    else if (data[2] == undefined) row.push([data[0], data[1]])
+    else row.push([data[0], data[1], data[2]]);
+   }
+  }
+  $scope.rows = row;
+ };
+ $scope.rows = [];
 });
